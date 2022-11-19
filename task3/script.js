30c0daacf48e3f211fa99e98e02c1891f31f89f5
grumich <gartlain@mail.ru>
2022-11-19 19:35:45 +0300

    Traffic Light modified

--- task3/script.js
+++ task3/script.js
@@ -16,3 +16,28 @@ function makeRed() {
   trafficLightEl.addEventListener("click", makeGreen);
 }
 trafficLightEl.addEventListener("click", makeGreen);
+
+
+
+const trafficLightEl1 = document.querySelector("#trafficLight1");
+const trafficLightEl2 = document.querySelector("#trafficLight2");
+const trafficLightEl3 = document.querySelector("#trafficLight3");
+
+function makeGreen1() {
+  trafficLightEl1.style.background = "green";
+  trafficLightEl2.style.background = "black";
+  trafficLightEl3.style.background = "black";
+}
+function makeYellow1() {
+  trafficLightEl2.style.background = "yellow";
+  trafficLightEl1.style.background = "black";
+  trafficLightEl3.style.background = "black";
+}
+function makeRed1() {
+  trafficLightEl3.style.background = "red";
+  trafficLightEl1.style.background = "black";
+  trafficLightEl2.style.background = "black";
+}
+trafficLightEl1.addEventListener("click", makeGreen1);
+trafficLightEl2.addEventListener("click", makeYellow1);
+trafficLightEl3.addEventListener("click", makeRed1);
